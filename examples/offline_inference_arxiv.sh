95315d3a55d96b8ee752798156bdc171bf2ae4f4
Lannie Hough <lannie.hough@protonmail.com>
2024-10-21 23:38:36 -0400

    fixed batch script for perlmutter

--- examples/offline_inference_arxiv.sh
+++ examples/offline_inference_arxiv.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-##SBATCH --job-name=batch_job_benchmark_vllm
-##SBATCH --nodes=1
-##SBATCH --time=01:00:00
-##SBATCH --constraint=gpu
-##SBATCH --gpus=4
-##SBATCH --account=m2404
-##SBATCH --output=batch_job_%j.out
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH -G 1
+#SBATCH -q regular
+#SBATCH -J vllm-benchmarking
+#SBATCH --time=<time>
+#SBATCH --mail-user=<user>@umd.edu
+#SBATCH --mail-type=ALL
+#SBATCH -A <account>
 
 module load python/3.10
 module load cudatoolkit/12.2
 
+cd ..
+source vllm_venv/bin/activate
+cd examples
+
 export HF_HOME="$SCRATCH/hf_cache"
 export TRANSFORMERS_HOME="$SCRATCH/hf_cache"
 export HF_DATASETS_CACHE="$SCRATCH/hf_cache"
+export HF_ACCESS_TOKEN="TOKEN_HERE"
 
-python3 offline_inference_arxiv.py > offline_inference_arxiv.log
+python3 offline_inference_arxiv.py offline_inference_arxiv.ini &> offline_inference_arxiv.log
