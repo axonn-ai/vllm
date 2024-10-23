4e9582d35111a7482e5d80a3f74a4ec9f37f2252
Prajwal Singhania <prajwal1210@gmail.com>
2024-10-23 00:01:50 -0400

    Throughput calculation fix; Added warmup and ability to toggle eager mode

--- examples/offline_inference_arxiv.py
+++ examples/offline_inference_arxiv.py
@@ -17,9 +17,14 @@ batch_sizes = from_csv(config["DEFAULT"]["batch_sizes"])
 prompt_lengths = from_csv(config["DEFAULT"]["prompt_lengths"])
 generation_lengths = from_csv(config["DEFAULT"]["generation_lengths"])
 tensor_parallel_sizes = from_csv(config["DEFAULT"]["tensor_parallel_sizes"])
+num_warmups = int(config["DEFAULT"]["num_warmups"])
 num_iterations = int(config["DEFAULT"]["num_iterations"])
 output_file = config["DEFAULT"]["output_file"]
 model = config["DEFAULT"]["model"]
+eager_mode = config.getboolean("DEFAULT", "eager_mode", fallback=False)
+
+print (config)
+
 
 ds = load_dataset(dataset, "section", split = "test")
 prompts = ds[config["DEFAULT"]["dataset_key"]]
@@ -41,7 +46,8 @@ def generate_with_params(llm, sampling_params, prompts, prompt_length, batch_siz
     time_generating_tokens = 0.0
     tokens_generated = 0
 
-    prompts = prompts[:num_iterations * batch_size]
+    prompts = prompts[:(num_warmups + num_iterations) * batch_size]
+    iter_num = 0
     for i in tqdm(range(0, len(prompts), batch_size)):
         batch_prompts = prompts[i : i + batch_size]
         batch_prompts_tokenized = []
@@ -52,16 +58,29 @@ def generate_with_params(llm, sampling_params, prompts, prompt_length, batch_siz
             tp = TokensPrompt(prompt_token_ids = ids)
             batch_prompts_tokenized.append(tp)
         outputs = llm.generate(batch_prompts_tokenized, sampling_params)
+
+        duration = 0
+        output_tokens_per_batch = 0
         for output in outputs:
             token_ids = output.outputs[0].token_ids
             token_count = len(token_ids)
             metrics = output.metrics
             start = metrics.first_token_time
             end = metrics.finished_time
-            duration = end - start
-            time_generating_tokens += duration
-            tokens_generated += token_count
+            duration += (end - start)
+            output_tokens_per_batch += token_count
 
+        duration = float(duration) / len(outputs)
+        if iter_num < num_warmups: # Skip the warmup iterations
+            pass
+        else:   
+            # TODO: Verify this. Essentially if we are processing a batch each duration should be exactly the same for the outputs of a single batch since the batch is processed in parallel
+            time_generating_tokens += duration
+            tokens_generated += output_tokens_per_batch
+
+        iter_num += 1
+
+    # TODO: I am not sure if we should find throughput per batch adn then average it across all iterations or do this
     tokens_per_sec = float(tokens_generated) / time_generating_tokens
     return tokens_per_sec
 
@@ -69,7 +88,7 @@ for ts in tensor_parallel_sizes:
     llm = LLM(
         model = model,
         tensor_parallel_size = ts,
-        enforce_eager = True
+        enforce_eager = eager_mode
     )
     for gl in generation_lengths:
         sampling_params = SamplingParams(
